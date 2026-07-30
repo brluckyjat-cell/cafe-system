e4c1391722fa3396162fb74b2ecef7f58d893f26
brluckyjat-cell <brluckyjat@gmail.com>
2026-07-30 22:22:03 +0530

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,14 +1,15 @@
+
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-auth.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBbxtIQ80kNC9f4rDAG42mo-r3All", 
+  apiKey: "AIzaSyBbxtIQ8OkNC9f4rDAG42mo-r3AllDr2YI",
   authDomain: "my-cafe-app-fb7ba.firebaseapp.com",
   projectId: "my-cafe-app-fb7ba",
   storageBucket: "my-cafe-app-fb7ba.firebasestorage.app",
   messagingSenderId: "227548526389",
-  appId: "1:227548526389:web:e20f674879277ddf",
+  appId: "1:227548526389:web:e20f674879277ddf12fe7c",
   measurementId: "G-SP32DJ8042"
 };
 
