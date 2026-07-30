76fa932854c504c9640eb2159041eb90c1338334
brluckyjat-cell <brluckyjat@gmail.com>
2026-07-30 21:45:06 +0530

    Add Firebase configuration and initialization

---
+++ firebase-config.js
@@ -0,0 +1,17 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-auth.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBbxtIQ80kNC9f4rDAG42mo-r3All", 
+  authDomain: "my-cafe-app-fb7ba.firebaseapp.com",
+  projectId: "my-cafe-app-fb7ba",
+  storageBucket: "my-cafe-app-fb7ba.firebasestorage.app",
+  messagingSenderId: "227548526389",
+  appId: "1:227548526389:web:e20f674879277ddf",
+  measurementId: "G-SP32DJ8042"
+};
+
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
+export const auth = getAuth(app);
